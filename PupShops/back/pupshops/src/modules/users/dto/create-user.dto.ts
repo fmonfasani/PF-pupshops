e21a123af6f1135aa5ad2610e519b96effbea523
Ignacio Jordan Coria <ignaciojordan17@gmail.com>
2024-09-11 19:51:40 -0300

    Cambios en configuracion BDD y entidad user

--- PupShops/back/pupshops/src/modules/users/dto/create-user.dto.ts
+++ PupShops/back/pupshops/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,109 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  IsInt,
+  Length,
+  IsStrongPassword,
+  IsBoolean,
+  IsEmpty,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  @ApiProperty({
+    description: 'Nombre (hasta 50 caracteres)',
+    example: 'Juan',
+  })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  @ApiProperty({
+    description: 'Apellido (hasta 50 caracteres)',
+    example: 'Sanchez',
+  })
+  lastname: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty({
+    description: 'Debe ser un email válido',
+    example: 'juansanchez@ejemplo.com',
+  })
+  email: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  @ApiProperty({
+    description:
+      'La contraseña debe contener al menos 1 mayúscula, 1 minúscula, 1 número y 1 caracter especial',
+    example: 'Contraseña1234-',
+  })
+  password: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  @ApiProperty({
+    description:
+      'Corrobora que la confirmación sea igual a la contraseña proporcionada',
+    example: 'Contraseña1234-',
+  })
+  confirmPassword: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsEmpty()
+  isAdmin: boolean;
+
+  @IsNotEmpty()
+  @IsInt()
+  @ApiProperty({
+    description: 'Agrega tu codigo de área ',
+    example: '543511111111',
+  })
+  phone: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 20)
+  @ApiProperty({
+    description: 'Máximo 20 caracteres',
+    example: 'Inglaterra',
+  })
+  country: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 20)
+  @ApiProperty({
+    description: 'Máximo 20 caracteres',
+    example: 'Londres',
+  })
+  city: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 80)
+  @ApiProperty({
+    description: 'Máximo 80 caracteres',
+    example: 'Calle 123',
+  })
+  address: string;
+}
