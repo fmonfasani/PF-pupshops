14b290ebab018b8bba674936b6cdf1df7894e2d8
fmonfasani <fmonfasani@gmail.com>
2024-09-11 18:33:36 -0300

    creacion appoint. services y orderdetails

--- PupShops/back/pupshops/src/credentials/credentials.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CredentialsService } from './credentials.service';
-import { CredentialsController } from './credentials.controller';
-
-@Module({
-  controllers: [CredentialsController],
-  providers: [CredentialsService],
-})
-export class CredentialsModule {}
--- PupShops/back/pupshops/src/credentials/dto/update-credential.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCredentialDto } from './create-credential.dto';
-
-export class UpdateCredentialDto extends PartialType(CreateCredentialDto) {}
