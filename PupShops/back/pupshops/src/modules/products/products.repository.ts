c7f5f3b96c58da4414d420234c905bd47b49b8b7
ConstanzaHerrera-code <conty1505@gmail.com>
2024-09-11 20:13:40 -0300

    Creacion de la entidad de Productos

--- PupShops/back/pupshops/src/modules/products/products.repository.ts
+++ PupShops/back/pupshops/src/modules/products/products.repository.ts
@@ -1,47 +1,21 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Products } from "./entities/product.entity";
+import { Repository } from "typeorm";
 
 
 
+@Injectable()
+export class ProductsRepository{
+    constructor(@InjectRepository(Products) private productsRepository: Repository<Products>){}
 
-@Entity({
-    name: 'products',
-})
-export class Products{
-    @PrimaryGeneratedColumn('uuid')
-    id:string;
+    getProducts(){}
 
+    getOneProduct(){}
 
-    @Column({
-        type: 'varchar',
-        length: 100,
-        unique:true,
-        nullable:false,
-    })
-    name: string;
+    addProducts(){}
 
-    @Column({
-        type: 'text',
-        nullable: false,
-    })
-    description:string;
+    updateProducts(){}
 
-    @Column({
-        type: 'decimal',
-        precision: 10,
-        scale:2,
-        nullable:false,
-    })
-    price:number;
-
-    @Column({
-        type: 'int',
-        nullable:false
-    })
-    stock:number;
-
-    @Column({
-        type: 'text',
-        default: 'http'
-    })
-    imgUrl: string;
+    deleteProducts(){}
 }
--- PupShops/back/pupshops/src/modules/products/products.service.ts
+++ PupShops/back/pupshops/src/modules/products/products.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
+  createProduct() {
     return 'This action adds a new product';
   }
 
-  findAll() {
+  findAllProducts() {
     return `This action returns all products`;
   }
 
-  findOne(id: number) {
+  findOneProduct(id: number) {
     return `This action returns a #${id} product`;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  updateProduct(id: number) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
+  removeProduct(id: number) {
     return `This action removes a #${id} product`;
   }
 }
