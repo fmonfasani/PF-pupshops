76500566abe4e0abc81bdb51d7156f175b28722c
fmonfasani <fmonfasani@gmail.com>
2024-09-12 22:35:10 -0300

    fix entities and up server

--- PupShops/back/pupshops/src/config/typeorm.ts
+++ PupShops/back/pupshops/src/config/typeorm.ts
@@ -1,23 +1,22 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+/* eslint-disable prettier/prettier */
+import { DataSource } from 'typeorm';
 import { config as dotenvConfig } from 'dotenv';
+import { DataSourceOptions } from 'typeorm';
 import { registerAs } from '@nestjs/config';
 
 dotenvConfig({ path: '.env.development' });
 
-const config = {
+const config: DataSourceOptions = {
   type: 'postgres',
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT as unknown as number,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  autoLoadEntities: true,
-  synchronize: true,
-  dropSchema: true,
-  logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*{.js,.ts}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: true,
 };
-
 export default registerAs('typeorm', () => config);
 export const connectionSource = new DataSource(config as DataSourceOptions);
