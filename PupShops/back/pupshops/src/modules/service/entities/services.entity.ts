129148ccb28dfa92dcf53607ed48a65915675af5
fmonfasani <fmonfasani@gmail.com>
2024-09-21 15:47:15 -0300

    Actualice service, mejore las funcionalidades y validaciones

--- PupShops/back/pupshops/src/modules/service/entities/services.entity.ts
+++ PupShops/back/pupshops/src/modules/service/entities/services.entity.ts
@@ -1,6 +1,13 @@
 /* eslint-disable prettier/prettier */
 // src/service/entities/service.entity.ts
-import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
 import { Appointment } from '../../appointments/entities/appointment.entity';
 
 @Entity('services')
@@ -8,9 +15,22 @@ export class Service {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: 'varchar', length: 50, unique: true })
   name: string;
 
   @OneToMany(() => Appointment, (appointment) => appointment.service)
   appointments: Appointment[];
+
+  // Columna para marcar la eliminación lógica
+  @Column({ type: 'timestamp', nullable: true })
+  deletedAt: Date | null;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
+  price: number;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  normalizeName() {
+    this.name = this.name.toLowerCase();
+  }
 }
