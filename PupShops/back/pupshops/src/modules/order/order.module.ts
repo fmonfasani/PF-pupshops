0cee1bbc483caed88575114745a98f255ab576bc
ConstanzaHerrera-code <conty1505@gmail.com>
2024-09-25 12:01:17 -0300

    cambios en orders

--- PupShops/back/pupshops/src/modules/order/order.module.ts
+++ PupShops/back/pupshops/src/modules/order/order.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Orders } from './entities/order.entity';
+import { OrderDetails } from './entities/order-detail.entity';
+import { Products } from '../products/entities/product.entity';
+import { User } from '../users/entities/user.entity';
+import { OrdersRepository } from './order.repository'; 
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Orders, OrderDetails, Products, User]),
+  ],
+  providers: [OrdersRepository, OrderService], 
   controllers: [OrderController],
-  providers: [OrderService],
 })
 export class OrderModule {}
