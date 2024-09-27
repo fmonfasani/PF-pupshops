64bbfb7a81077ad793255706295c7ad7bf6cb55e
Cararo Emiliano <cararoemilianofrontend@gmail.com>
2024-09-27 17:36:40 -0300

    cambio-auth

--- PupShops/front/src/helpers/auth.helpers.ts
+++ PupShops/front/src/helpers/auth.helpers.ts
@@ -1,40 +1,65 @@
 import { ILoginProps, IRegisterProps } from "../Interfaces/types";
 
 const APIURL = process.env.NEXT_PUBLIC_API_URL;
+
+// Función para registrar un usuario
 export async function register(userData: IRegisterProps) {
   try {
-    const res = await fetch(${APIURL}/users/register, {
+    // Verificar si APIURL está definida
+    if (!APIURL) {
+      throw new Error("APIURL no está definida");
+    }
+
+    const res = await fetch(`${APIURL}/users/register`, {
+      
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(userData)
+      body: JSON.stringify(userData),
     });
+
     if (res.ok) {
       return res.json();
     } else {
-      throw Error("Failed to register email already exists");
+      const errorText = await res.text(); // Captura la respuesta como texto
+      throw new Error(`Failed to register: ${errorText}`);
     }
-  } catch (error: any) {
-    throw new Error(error);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(`Error en el registro: ${error.message}`);
+    }
+    throw new Error("Error en el registro: Ocurrió un problema inesperado.");
   }
 }
 
+// Función para iniciar sesión
 export async function login(userData: ILoginProps) {
   try {
-    const res = await fetch(${APIURL}/auth/signin, {
+    // Verificar si APIURL está definida
+    if (!APIURL) {
+      throw new Error("APIURL no está definida");
+    }
+
+    const res = await fetch(`${APIURL}/auth/signin`, {
+      // Aquí se corrige la interpolación
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(userData)
+      body: JSON.stringify(userData),
     });
+
     if (res.ok) {
       return res.json();
     } else {
-      throw Error("Failed to Login");
+      const errorText = await res.text(); // Captura la respuesta como texto
+      throw new Error(`Failed to login: ${errorText}`);
     }
-  } catch (error: any) {
-    throw new Error(error);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(`Error en el login: ${error.message}`);
+    }
+    throw new Error("Error en el login: Ocurrió un problema inesperado.");
   }
 }
