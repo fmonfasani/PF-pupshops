79adaacfab65d18ec6ab3f22db3477d9ab07e4c6
Ignacio <ignaciojordan17@gmail.com>
2024-09-27 20:53:49 -0300

    auth0

--- PupShops/back/pupshops/src/main.ts
+++ PupShops/back/pupshops/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import {config as auth0config} from './config/auth0.config'
+import { auth } from 'express-openid-connect';
 const port = 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(auth(auth0config))
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
