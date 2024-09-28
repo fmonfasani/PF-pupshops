d385767c5ec0b02ef7d421a1c013382014aa2aff
Ignacio <ignaciojordan17@gmail.com>
2024-09-28 12:34:41 -0300

    borrado inteligente

--- PupShops/back/pupshops/src/modules/auth/auth.guard.ts
+++ PupShops/back/pupshops/src/modules/auth/auth.guard.ts
@@ -4,6 +4,7 @@ import {
   ExecutionContext,
   Injectable,
   UnauthorizedException,
+  ForbiddenException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
@@ -11,6 +12,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private readonly jwtService: JwtService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -20,13 +22,20 @@ export class AuthGuard implements CanActivate {
     if (!token) {
       throw new BadRequestException('Token de autenticación erróneo/faltante');
     }
+
     try {
       const secret = process.env.JWT_SECRET;
       const payload = this.jwtService.verify(token, { secret });
 
+      
+      if (!payload.isActive===false) {
+        throw new ForbiddenException('Este usuario está inactivo y no puede acceder a esta ruta');
+      }
+
       payload.iat = new Date(payload.iat * 1000);
       payload.exp = new Date(payload.exp * 1000);
       request.user = payload;
+
       return true;
     } catch (error) {
       throw new UnauthorizedException('Token Inválido');
