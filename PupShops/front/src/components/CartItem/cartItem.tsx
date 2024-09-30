186a87fbdafc7d87ab1567b96cc1d7c054266de3
Cararo Emiliano <cararoemilianofrontend@gmail.com>
2024-09-30 06:38:45 -0300

    carroFinal

--- PupShops/front/src/components/CartItem/cartItem.tsx
+++ PupShops/front/src/components/CartItem/cartItem.tsx
@@ -3,7 +3,7 @@
 import React, { useContext } from "react";
 import { cartContext } from "@/context/cartContext";
 import { IProduct } from "@/Interfaces/ICart";
-import Image from "next/image"; // Importa el componente Image de Next.js
+import Image from "next/image";
 import { FaTrash } from "react-icons/fa"; // Asegúrate de tener react-icons instalado
 
 interface CartItemProps {
@@ -22,23 +22,26 @@ const CartItem: React.FC<CartItemProps> = ({ item }) => {
       <Image
         src={item.imgUrl}
         alt={item.name}
-        width={100} // Aumenta el tamaño de la imagen
-        height={100} // Aumenta el tamaño de la imagen
+        width={100}
+        height={100}
         className="rounded-md mr-4"
       />
       <div className="flex-grow">
         <h3 className="text-lg font-semibold">{item.name}</h3>
         <p className="text-gray-600">{item.description}</p>
-        <p className="text-lg font-bold">
+        <p className="text-lg font-light">
           Precio:{" "}
           <span className="text-teal-600">${item.price.toFixed(2)}</span>
+        </p>
+        <p className="text-lg font-light">
+          Cantidad: <span className="text-teal-600">{item.quantity || 1}</span>
         </p>
       </div>
       <button
         onClick={handleRemoveClick}
         className="ml-4 text-teal-600 hover:text-orange-300 transition"
       >
-        <FaTrash size={24} /> {/* Icono de papelera */}
+        <FaTrash size={24} />
       </button>
     </div>
   );
